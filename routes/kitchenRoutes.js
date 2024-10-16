428fcec5744dd326e697afb19fc194d72640d896
Mohamad-Za <mohammad.zahedi230@gmail.com>
2024-10-16 22:24:03 +0200

    w

--- routes/kitchenRoutes.js
+++ routes/kitchenRoutes.js
@@ -10,7 +10,6 @@ router.get('/about', (req, res) => {
     data.title = "About";
     
     data.team = [
-        { name: "Mohamad Zahedi", githubUrl: "https://github.com/Mohamad-Za", avatarUrl: "https://github.com/Mohamad-Za.png" },
         { name: "Member 2", githubUrl: "https://github.com/member2-profile", avatarUrl: "https://github.com/member2-profile.png" },
         { name: "Member 3", githubUrl: "https://github.com/member3-profile", avatarUrl: "https://github.com/member3-profile.png" },
         { name: "Member 4", githubUrl: "https://github.com/member4-profile", avatarUrl: "https://github.com/member4-profile.png" },
